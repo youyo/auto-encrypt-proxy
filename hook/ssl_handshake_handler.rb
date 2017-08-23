764201c6d9a628aa21c373bd6006e78c704c1981
youyo <1003ni2@gmail.com>
2017-08-23 17:34:59 +0900

    update

--- hook/ssl_handshake_handler.rb
+++ hook/ssl_handshake_handler.rb
@@ -1,17 +1,16 @@
 ssl = Nginx::SSL.new
 domain = ssl.servername
-redis = Userdata.new.redis
+redis = Redis.new 'redis', 6379
 allow_domain = []
 
 acme = Nginx::SSL::ACME::Client.new(
-  Userdata.new.endpoint,
+  userdata.endpoint,
   domain,
   redis,
   false,
   allow_domain
 )
 
-# raise 'not allowed servername' unless acme.allow_domain?
 raise 'not allowed servername' unless redis.exists?(domain)
 
 if redis["#{domain}.crt"].nil? || redis["#{domain}.key"].nil?
@@ -22,7 +21,7 @@ if redis["#{domain}.crt"].nil? || redis["#{domain}.key"].nil?
     #{domain}_private_key
     #{domain}.key
     #{domain}.crt
-  ].each { |key| redis.expire key, Userdata.new.ttl }
+  ].each { |key| redis.expire key, userdata.ttl }
 end
 
 ssl.certificate_data = redis["#{domain}.crt"]
