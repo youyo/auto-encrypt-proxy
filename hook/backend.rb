a64b21470419afd6e6a18536611b2c1221bd97ea
youyo <1003ni2@gmail.com>
2017-08-23 18:23:02 +0900

    update

--- hook/backend.rb
+++ hook/backend.rb
@@ -1,4 +1,5 @@
 domain = Nginx::Request.new.hostname
 redis = Redis.new 'redis', 6379
+raise 'not allowed servername' unless redis.exists?(domain)
 servers = redis.lrange domain, 0, -1
 servers[rand(servers.length)]
